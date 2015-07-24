2525a0663928d34f9a135e65711219aacf00a70e
Jean Chassoul <chassoul@gmail.com>
2015-07-24 15:18:29 -0600

    go

--- server.py
+++ server.py
@@ -105,7 +105,7 @@ def periodic_records_callback():
 
             logging.info('checked SQL uniqueid')
 
-            logging.info(record)
+            logging.info(record.get('uniqueid'))
 
             flag = yield periodic.assign_record(
                 db,
