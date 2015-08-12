ff81f5d288128e83847c2e4cbd64c451ffa22660
Jean Chassoul <chassoul@gmail.com>
2015-08-12 13:25:32 -0600

    ok without exit

--- mango/system/__init__.py
+++ mango/system/__init__.py
@@ -104,14 +104,22 @@ def server_push(port="5556"):
     socket = context.socket(zmq.PUSH)
     socket.bind("tcp://*:%s" % port)
     print "Running server on port: ", port
+    message = 'Continue'
     # serves only 5 request and dies
-    for reqnum in range(10):
-        if reqnum < 6:
-            socket.send("Continue")
-        else:
-            socket.send("Exit")
-            break
-        time.sleep (1)
+    while message == 'Continue':
+        socket.send(message)
+        time.sleep(1)
+    else:
+        socket.send("Exit")
+        break
+
+    #for reqnum in range(10):
+    #    if reqnum < 6:
+    #        socket.send(message)
+    #    else:
+    #        socket.send("Exit")
+    #        break
+    #    time.sleep(1)
 
 def server_pub(port="5558"):
     '''
