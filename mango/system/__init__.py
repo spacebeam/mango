46ea37786f38c06af05394818182fe3fafe7862b
Jean Chassoul <chassoul@gmail.com>
2015-08-13 09:24:24 -0600

    wut change

--- mango/system/__init__.py
+++ mango/system/__init__.py
@@ -159,8 +159,8 @@ def server_pub(port="5558"):
         # Wait for next request from client
         topic = random.randrange(8,10)
         messagedata = "server#{0}".format(publisher_id)
-        logging.warning("Server publisher_id {0} publish message {1}".format(publisher_id, message))
         message = "{0} {1}".format(topic, messagedata)
+        logging.warning("Server publisher_id {0} publish message {1}".format(publisher_id, message))
         socket.send(message)
         time.sleep(1)
 
