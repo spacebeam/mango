719756ea5191455e1a951e2fb9ec38aebf355aaf
Jean Chassoul <chassoul@gmail.com>
2015-11-17 10:01:41 -0600

    message not events

--- server.py
+++ server.py
@@ -153,7 +153,7 @@ def periodic_ws_send():
     '''
         Periodic websocket send
     '''
-    message = {'event':'ping'}
+    message = {'message':'heartbeat'}
     message = json.dumps(message)
     wsSend(message)
 
