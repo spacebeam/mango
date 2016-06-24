2bf53c6317d78e5c0263836b1e7750c9bba7f499
Jean Chassoul <chassoul@gmail.com>
2016-06-24 16:06:03 -0400

    q?

--- mango/messages/__init__.py
+++ mango/messages/__init__.py
@@ -12,10 +12,14 @@ __author__ = 'Jean Chassoul'
 
 
 import uuid
+import uuid as _id
 
 from schematics import models
 from schematics import types
 from schematics.types import compound
+
+from mango.messages import tasks
+from mango.messages import records
 
 
 class BaseResult(models.Model):
@@ -44,3 +48,71 @@ class Resource(models.Model):
     addresses = compound.ModelType(SimpleResource)
 
     total = types.IntType()
+
+
+class BaseResult(models.Model):
+    '''
+        base result
+    '''
+    count = types.IntType()
+    page = types.IntType()
+    results = compound.ListType(compound.ModelType(records.Record))
+
+
+class BaseHistory(models.Model):
+    '''
+        base history
+
+        Scopes: accounts, numbers
+    '''
+    uuid = types.UUIDType(default=_id.uuid4)
+    record_uuid = types.UUIDType(default=_id.uuid4)
+    ban =  types.UUIDType(default=_id.uuid4) # 'billing account number'
+    country_number = types.IntType()
+    number = types.StringType()
+    campaign_uuid = types.UUIDType(default=_id.uuid4)
+    start_time = types.DateTimeType()
+    caller_id = types.StringType()
+    caller_id_name = types.StringType()
+    origin_number = types.StringType()
+    origin_city = types.StringType()
+    origin_state = types.StringType()
+    origin_province = types.StringType()
+    destination_type = types.IntType()
+    destination_contry_code = types.StringType()
+    destination_number = types.StringType()
+    destination_city = types.StringType()
+    destination_state = types.StringType()
+    duration = types.IntType()
+    duration_unrounded = types.IntType()
+    cost = types.IntType()
+    special_rate = types.IntType()
+    lead_type = types.IntType()
+    web_call = types.BooleanType(default=False)
+
+
+class ConectedDuration(models.Model):
+    '''
+        connected duration
+    '''
+    uuid = types.UUIDType(default=_id.uuid4)
+    customer_uuid = types.UUIDType(default=_id.uuid4)
+    phone_1 = types.StringType()
+    first_name = types.StringType()
+    rep_phone = types.StringType()
+    keyword = types.StringType()
+    duration_unrounded = types.IntType()
+    record_uuid = types.UUIDType(default=_id.uuid4)
+
+
+class VoiceCall(models.Model):
+    '''
+        voice call
+    '''
+    uuid = types.UUIDType(default=_id.uuid4)
+    account_uuid = types.UUIDType(default=_id.uuid4)
+    phone_1 = types.StringType()
+    first_name = types.StringType()
+    last_name = types.StringType()
+    rep_phone = types.StringType()
+    keyword = types.StringType()
--- mango/system/records.py
+++ mango/system/records.py
@@ -328,6 +328,116 @@ class Records(object):
         raise gen.Return(result)
 
     @gen.coroutine
+    def get_cost_summary(self, account, routes, lapse, start, end):
+        '''
+            get_cost_summary
+        '''
+
+        if not start:
+            start = arrow.utcnow()
+        if not end:
+            end = start.replace(days=+1)
+
+        start = start.timestamp
+
+        # TODO: multiple routes, getting there..
+        single_route = routes
+
+        # MongoDB aggregation match operator
+        if type(account) is list:
+            match = {
+                'assigned': True,
+                'start': {'$gte':start, '$lt':end},
+                'channel': {'$regex': single_route['channel']},
+                'dstchannel': {'$regex': single_route['dstchannel']},
+                '$or': [{'accountcode':a} for a in account]
+            }
+        else:
+            match = {
+                'accountcode': account, 
+                'assigned': True,
+                'start': {'$gte':start, '$lt': end},
+                'channel': {'$regex': single_route['channel']},
+                'dstchannel': {'$regex': single_route['dstchannel']},
+            }
+    
+        # MongoDB aggregation project operator
+        project = {
+               "_id" : 0,
+               # record duration seconds
+               "duration" : 1,
+               # record billing seconds
+               "billsec" : 1,
+               # record times
+               "start" : 1,
+               'answer': 1,
+               'end': 1,
+        
+               "year" : {
+                         "$year" : "$start"
+               },
+               "month" : {
+                          "$month" : "$start"
+               },
+               "week" : {
+                         "$week" : "$start"
+               },
+               "day" : {  
+                        "$dayOfMonth" : "$start"
+               },
+               "hour" : {  
+                         "$hour" : "$start"
+               },
+               "minute" : {  
+                           "$minute" : "$start"
+               },
+               "second" : {
+                           "$second" : "$start"
+               }
+        }
+        
+        # MongoDB aggregation group operator
+        group = {
+            '_id': {
+                'start': '$start',
+                'answer': '$answer',
+                'end': '$end',
+                
+                'year': '$year',
+                'month': '$month',
+                'week':'$week',        
+                'day': '$day',
+                'hour':'$hour',
+                'minute': '$minute',
+                'second': '$second',
+            },
+            'records': {
+                '$sum': 1
+            },
+            'average': {
+                '$avg':'$seconds'
+            },
+            'duration': {
+                '$sum':'$duration'
+            },
+            'seconds': {
+                '$sum':'$seconds'
+            }
+        }
+        
+        # MongoDB aggregation pipeline
+        pipeline = [
+            {'$match':match},
+            {'$project':project},
+            {'$group':group}
+        ]
+        
+
+        result = yield self.db.records.aggregate(pipeline)
+
+        raise gen.Return(result.get('result'))
+
+    @gen.coroutine
     def remove_record(self, record_uuid):
         '''
             Remove a record entry
