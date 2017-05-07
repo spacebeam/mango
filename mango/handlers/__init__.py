1bbaa6fa6adb5d5b26772e71cc3f29adbb98a68a
Jean Chassoul <chassoul@gmail.com>
2017-05-07 13:36:54 -0400

    ok

--- mango/handlers/__init__.py
+++ mango/handlers/__init__.py
@@ -75,7 +75,8 @@ class BaseHandler(web.RequestHandler):
         '''
             Mango default headers
         '''
-        self.set_header("Access-Control-Allow-Origin", self.settings['domain'])
+        #self.set_header("Access-Control-Allow-Origin", 'self.settings['domain']')
+        self.set_header("Access-Control-Allow-Origin", '*')
 
     def get_current_username(self):
         '''
