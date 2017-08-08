34902af42caac26546cdffe4b1ae30614e1bc61b
Anthony Solorzano Lopez <anthony06891@gmail.com>
2017-08-07 19:32:18 -0600

    update

--- mango/structures/accounts.py
+++ mango/structures/accounts.py
@@ -28,27 +28,48 @@ class AccountMap(object):
         struct
     ):
         '''
-            Accounts event from your white-headed capuchins.
+            Account map structure
         '''
         bucket = client.bucket_type(bucket_type).bucket('{0}'.format(bucket_name))
         bucket.set_properties({'search_index': search_index})
-
-        # now we define or map data structure
         self.map = Map(bucket, None)
+        # start of map structure
         self.map.registers['uuid'].assign(struct.get('uuid', ''))
         self.map.registers['account'].assign(struct.get('account', ''))
+        self.map.registers['first_name'].assign(struct.get('first_name', ''))
+        self.map.registers['last_name'].assign(struct.get('last_name', ''))
+        self.map.registers['type'].assign(struct.get('type', ''))
+        self.map.registers['account_type'].assign(struct.get('account_type', ''))
+        self.map.registers['permission'].assign(struct.get('permission', ''))
+        self.map.registers['orgs'].assign(struct.get('orgs', ''))
+        self.map.registers['password'].assign(struct.get('password', ''))
+        self.map.registers['members'].assign(struct.get('members', ''))
+        self.map.registers['teams'].assign(struct.get('teams', ''))
+        self.map.registers['labels'].assign(struct.get('labels', ''))
         self.map.registers['status'].assign(struct.get('status', ''))
-        self.map.registers['created_at'].assign(struct.get('created_at', ''))
-        self.map.registers['created_by'].assign(struct.get('created_by', ''))
+        self.map.registers['email'].assign(struct.get('email', ''))
+        self.map.registers['phone_number'].assign(struct.get('phone_number', ''))
+        self.map.registers['extension'].assign(struct.get('extension', ''))
+        self.map.registers['country_code'].assign(struct.get('country_code', ''))
+        self.map.registers['timezone'].assign(struct.get('timezone', ''))
+        self.map.registers['company'].assign(struct.get('company', ''))
+        self.map.registers['location'].assign(struct.get('location', ''))
+        self.map.registers['membership'].assign(struct.get('membership', ''))
+        self.map.registers['resources'].assign(struct.get('resources', ''))
+        self.map.registers['phones'].assign(struct.get('phones', ''))
+        self.map.registers['emails'].assign(struct.get('emails', ''))
+        self.map.registers['max_channels'].assign(struct.get('max_channels', ''))
+        self.map.registers['is_admin'].assign(struct.get('is_admin', ''))
+        self.map.registers['checked'].assign(struct.get('checked', ''))
+        self.map.registers['checked_by'].assign(struct.get('checked_by', ''))
         self.map.registers['updated_by'].assign(struct.get('updated_by', ''))
         self.map.registers['updated_at'].assign(struct.get('updated_at', ''))
-        self.map.registers['change_history '].assign(struct.get('change_history', ''))
-        self.map.registers['tags'].assign(struct.get('tags', ''))
-        self.map.registers['snapshots'].assign(struct.get('snapshots', ''))
-        self.map.registers['addresses'].assign(struct.get('addresses', ''))
-        # and finally we store or CRDT datatype inside our database
+        self.map.registers['created_at'].assign(struct.get('created_at', ''))
+        self.map.registers['login_at'].assign(struct.get('login_at', ''))
+        self.map.registers['logout_at'].assign(struct.get('logout_at', ''))
+        self.map.registers['url'].assign(struct.get('url', ''))
+        # end of the map stuff
         self.map.store()
-
 
     @property
     def uuid(self):
@@ -61,33 +82,79 @@ class AccountMap(object):
     def to_json(self):
         event = self.map.reload()
         struct = {
-            "uuid":event.registers['uuid'].value,
-            "account":event.registers['account'].value,
-            "status":event.registers['status'].value,
-            "created_at":event.registers['created_at'].value,
-            "created_by":event.registers['created_by'].value,
-            "updated_by":event.registers['updated_by'].value,
-            "updated_at":event.registers['updated_at'].value,
-            "change_history ":event.registers['change_history'].value,
-            "tags":event.registers['tags'].value,
-            "snapshots":event.registers['snapshots'].value,
-            "addresses":event.registers['addresses'].value,
+            "uuid": event.registers['uuid'].value,
+            "account": event.registers['account'].value,
+            "first_name": event.registers['first_name'].value,
+            "last_name": event.registers['last_name'].value,
+            "type": event.registers['type'].value,
+            "account_type": event.registers['account_type'].value,
+            "permission": event.registers['permission'].value,
+            "orgs": event.registers['orgs'].value,
+            "password": event.registers['password'].value,
+            "members": event.registers['members'].value,
+            "teams": event.registers['teams'].value,
+            "labels": event.registers['labels'].value, 
+            "status": event.registers['status'].value,
+            "email": event.registers['email'].value,
+            "phone_number": event.registers['phone_number'].value,
+            "extension": event.registers['extension'].value,
+            "country_code": event.registers['country_code'].value,
+            "timezone": event.registers['timezone'].value,
+            "company": event.registers['company'].value,
+            "location": event.registers['location'].value,
+            "membership": event.registers['membership'].value,
+            "resources": event.registers['resources'].value,
+            "phones": event.registers['phones'].value,
+            "emails": event.registers['emails'].value,
+            "max_channels": event.registers['max_channels'].value,
+            "is_admin": event.registers['is_admin'].value,
+            "checked": event.registers['checked'].value,
+            "checked_by": event.registers['checked_by'].value,
+            "updated_by": event.registers['updated_by'].value,
+            "updated_at": event.registers['updated_at'].value,
+            "created_at": event.registers['created_at'].value,
+            "login_at": event.registers['login_at'].value,
+            "logout_at": event.registers['logout_at'].value,
+            "url": event.registers['url'].value,
         }
         return json.dumps(struct)
 
     def to_dict(self):
         event = self.map.reload()
         struct = {
-            "uuid":event.registers['uuid'].value,
-            "account":event.registers['account'].value,
-            "status":event.registers['status'].value,
-            "created_at":event.registers['created_at'].value,
-            "created_by":event.registers['created_by'].value,
-            "updated_by":event.registers['updated_by'].value,
-            "updated_at":event.registers['updated_at'].value,
-            "change_history ":event.registers['change_history'].value,
-            "tags":event.registers['tags'].value,
-            "snapshots":event.registers['snapshots'].value,
-            "addresses":event.registers['addresses'].value,
+            "uuid": event.registers['uuid'].value,
+            "account": event.registers['account'].value,
+            "first_name": event.registers['first_name'].value,
+            "last_name": event.registers['last_name'].value,
+            "type": event.registers['type'].value,
+            "account_type": event.registers['account_type'].value,
+            "permission": event.registers['permission'].value,
+            "orgs": event.registers['orgs'].value,
+            "password": event.registers['password'].value,
+            "members": event.registers['members'].value,
+            "teams": event.registers['teams'].value,
+            "labels": event.registers['labels'].value, 
+            "status": event.registers['status'].value,
+            "email": event.registers['email'].value,
+            "phone_number": event.registers['phone_number'].value,
+            "extension": event.registers['extension'].value,
+            "country_code": event.registers['country_code'].value,
+            "timezone": event.registers['timezone'].value,
+            "company": event.registers['company'].value,
+            "location": event.registers['location'].value,
+            "membership": event.registers['membership'].value,
+            "resources": event.registers['resources'].value,
+            "phones": event.registers['phones'].value,
+            "emails": event.registers['emails'].value,
+            "max_channels": event.registers['max_channels'].value,
+            "is_admin": event.registers['is_admin'].value,
+            "checked": event.registers['checked'].value,
+            "checked_by": event.registers['checked_by'].value,
+            "updated_by": event.registers['updated_by'].value,
+            "updated_at": event.registers['updated_at'].value,
+            "created_at": event.registers['created_at'].value,
+            "login_at": event.registers['login_at'].value,
+            "logout_at": event.registers['logout_at'].value,
+            "url": event.registers['url'].value,
         }
         return struct
