1511dae0b30ccc0a8c8a694a553c0d1e8b85c9c1
Jean Chassoul <chassoul@gmail.com>
2017-09-25 15:50:22 -0400

    no

--- mango/handlers/__init__.py
+++ mango/handlers/__init__.py
@@ -52,12 +52,11 @@ class BaseHandler(web.RequestHandler):
         '''
         self.set_header("Access-Control-Allow-Origin", self.settings.get('domain', '*'))
 
-    def get_current_username(self):
+    def get_username_cookie(self):
         '''
             Return the username from a secure cookie (require cookie_secret)
         '''
-        #return self.get_secure_cookie('username')
-        return False
+        return self.get_secure_cookie('username')
 
     @gen.coroutine
     def let_it_crash(self, struct, scheme, error, reason):
@@ -87,162 +86,7 @@ class BaseHandler(web.RequestHandler):
                 'error': u'nonsense',
                 'message': u'there is no error'
             }
-        # return message
         raise gen.Return(message)
-
-    # TODO: MAE! please refactor into smaller independent functions (=
-
-    @gen.coroutine
-    def new_sip_account(self, struct):
-        '''
-            New sip account for asterisk real-time
-        '''
-        # let's still support asterisk 11 for now!
-        message = 'nothing to see here'
-        try:
-            # Get SQL database from system settings
-            sql = self.settings.get('sql')
-            # PostgreSQL insert new sip account query
-            query = '''
-                insert into sip (
-                    name,
-                    defaultuser,
-                    fromuser,
-                    fromdomain,
-                    host,
-                    sippasswd,
-                    directmedia,
-                    videosupport,
-                    transport,
-                    allow,
-                    context,
-                    nat,
-                    qualify,
-                    avpf,
-                    encryption,
-                    force_avp,
-                    dtlsenable,
-                    dtlsverify,
-                    dtlscertfile,
-                    dtlsprivatekey,
-                    dtlssetup,
-                    directrtpsetup,
-                    icesupport
-                ) values (
-                    '{0}',
-                    '{1}',
-                    '{2}',
-                    '{3}',
-                    'dynamic',
-                    '{4}',
-                    'no',
-                    'no',
-                    'udp,wss',
-                    'opus,ulaw,alaw',
-                    'fun-accounts',
-                    'force_rport,comedia',
-                    'yes',
-                    'yes',
-                    'yes',
-                    'yes',
-                    'yes',
-                    'no',
-                    '/etc/asterisk/keys/asterisk.pem',
-                    '/etc/asterisk/keys/asterisk.pem',
-                    'actpass',
-                    'no',
-                    'yes'
-                );
-            '''.format(
-                struct.get('account'),
-                struct.get('account'),
-                struct.get('account'),
-                struct.get('domain', self.settings.get('domain')),
-                struct.get('password')
-            )
-            result = yield sql.query(query)
-            if result:
-                message = {'ack': True}
-            else:
-                message = {'ack': False}
-            result.free()
-            logging.warning('new sip real-time account for asterisk 11 spawned on postgresql {0}'.format(message))
-        
-        except Exception, e:
-            message = str(e)
-
-        # let's support asterisk 13 and the beast that pjsip chaims the be!
-        try:
-            # Get SQL database from system settings
-            sql = self.settings.get('sql')
-            # PostgreSQL insert new sip account
-            query = '''
-                insert into ps_aors(id, max_contacts)
-                values ('{0}', 1);
-            '''.format(
-                struct.get('account')
-            )
-            result = yield sql.query(query)
-            if result:
-                message = {'ack': True}
-            else:
-                message = {'ack': False}
-            result.free()
-            logging.warning('new pjsip account (1/3)')
-            query = '''
-                insert into ps_auths(id, auth_type, password, username)
-                values ('{0}', 'userpass', '{1}', '{2}');
-            '''.format(
-                struct.get('account'),
-                struct.get('password'),
-                struct.get('account')
-            )
-            result = yield sql.query(query)
-            if result:
-                message = {'ack': True}
-            else:
-                message = {'ack': False}
-            result.free()
-            logging.warning('new pjsip account (2/3)')
-            query = '''
-                insert into ps_endpoints (id, transport, aors, auth, context, disallow, allow, direct_media)
-                values ('{0}', 'transport-udp', '{1}', '{2}', 'fun-accounts', 'all', 'g722,ulaw,alaw,gsm', 'no');
-            '''.format(
-                struct.get('account'),
-                struct.get('account'),
-                struct.get('account')
-            )
-            result = yield sql.query(query)
-            if result:
-                message = {'ack': True}
-            else:
-                message = {'ack': False}
-            result.free()
-            logging.warning('new pjsip account (3/3)')
-            # additional ack information.
-            logging.warning('new pjsip real-time account for asterisk 13 spawned on postgresql {0}'.format(message))
-        except Exception, e:
-            message = str(e)
-
-        raise gen.Return(message)
-        
-    @gen.coroutine
-    def new_coturn_account(self, struct):
-        '''
-            New coturn account task
-        '''
-        try:
-            task = _tasks.Task(struct)
-            task.validate()
-        except Exception, e:
-            logging.exception(e)
-            raise e
-
-        task = clean_structure(task)
-        result = yield self.db.tasks.insert(task)
-
-        raise gen.Return(task.get('uuid'))
-
 
 @basic_authentication
 class LoginHandler(BaseHandler):
@@ -273,14 +117,14 @@ class LoginHandler(BaseHandler):
             self.set_status(200)
             self.finish(message)
         else:
-            # 401 status code?
+            # 401 status code ?
+            # I don't know, why 401 ?
             self.set_status(403)
             self.set_header('WWW-Authenticate', 'Basic realm=mango')
             self.finish()
             
     @gen.coroutine
     def options(self):
-        logging.warning(self.request.headers)
         self.set_header('Access-Control-Allow-Origin','*')
         self.set_header('Access-Control-Allow-Methods','GET, OPTIONS')
         self.set_header('Access-Control-Allow-Headers','Content-Type, Authorization')
