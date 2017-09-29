b13b8890e0b47c19b7802c0f264dc2ba6b24969e
Anthony Solorzano Lopez <anthony06891@gmail.com>
2017-09-28 22:00:23 -0600

    update

--- mango/handlers/__init__.py
+++ mango/handlers/__init__.py
@@ -137,7 +137,7 @@ class LoginHandler(BaseHandler):
         # resource parameters
         parameters = {}
         # mock your stuff
-        stuff = models.User.get_mock_object().to_primitive()
+        stuff = models.Account.get_mock_object().to_primitive()
         for k, v in stuff.items():
             if v is None:
                 parameters[k] = str(type('none'))
