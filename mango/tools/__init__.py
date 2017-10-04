eccdb66e4063e2bbd0cc04b84524142806b82241
Anthony Solorzano Lopez <anthony06891@gmail.com>
2017-10-04 17:24:49 -0600

    update

--- mango/tools/__init__.py
+++ mango/tools/__init__.py
@@ -115,7 +115,52 @@ def check_account_type(self, account, account_type):
     raise gen.Return(message)
 
 @gen.coroutine
-def get_account_uuid(self, account, password):
+def get_account_uuid(self, account):
+    '''
+        Get valid account uuid
+    '''
+    search_index = 'mango_account_index'
+    query = 'account_register:{0}'.format(account)
+    filter_query = 'account_register:{0}'.format(account)
+    # parse and build url
+    url = "https://{0}/search/query/{1}?wt=json&q={2}&fq={3}".format(
+        self.solr, search_index, query, filter_query
+    )
+    got_response = []
+    # clean response message
+    message = {}
+    def handle_request(response):
+        '''
+            Request Async Handler
+        '''
+        if response.error:
+            logging.error(response.error)
+            got_response.append({'error':True, 'message': response.error})
+        else:
+            got_response.append(json.loads(response.body))
+    try:
+        http_client.fetch(
+            url,
+            callback=handle_request
+        )
+        while len(got_response) == 0:
+            yield gen.sleep(0.0020) # don't be careless with the time.
+        stuff = got_response[0]
+        if stuff['response']['numFound']:
+            response_doc = stuff['response']['docs'][0]
+            IGNORE_ME = ["_yz_id","_yz_rk","_yz_rt","_yz_rb"]
+            message = dict(
+                (key.split('_register')[0], value)
+                for (key, value) in response_doc.items()
+                if key not in IGNORE_ME
+            )
+    except Exception, e:
+        logging.exception(e)
+        raise gen.Return(e)
+    raise gen.Return(message.get('uuid', 'not found'))
+
+@gen.coroutine
+def get_auth_uuid(self, account, password):
     '''
         Get valid account uuid
     '''
