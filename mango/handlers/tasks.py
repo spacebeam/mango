440d3f5c5d84f320767aca5966ebbd0355c14573
Anthony Solorzano Lopez <anthony06891@gmail.com>
2017-10-11 17:36:21 -0600

    update

--- mango/handlers/tasks.py
+++ mango/handlers/tasks.py
@@ -20,7 +20,7 @@ from mango.messages import tasks as models
 from mango.system import tasks
 from tornado import httpclient
 from mango.tools import errors, str2bool, check_json # <!--------------------   NEW RESOURCE
-from mango.handlers import BaseHandler, new_resource
+from mango.handlers import BaseHandler
 
 
 class Handler(tasks.Tasks, BaseHandler):
