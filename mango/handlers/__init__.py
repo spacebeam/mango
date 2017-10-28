bbcb2535d449052ca3b7308db82f48ed3017501a
Anthony Solorzano Lopez <anthony06891@gmail.com>
2017-10-27 19:50:08 -0600

    update ok no result

--- mango/handlers/__init__.py
+++ mango/handlers/__init__.py
@@ -17,7 +17,6 @@ import ujson as json
 from tornado import gen
 from tornado import web
 from mango.system import basic_authentication
-from mango.messages import tasks as _tasks
 from mango.messages import accounts as models
 from mango.tools import clean_structure, validate_uuid4
 from mango import errors
