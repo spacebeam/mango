038bde7d15cedd051a91b6bd69ee01d674cf3f37
Anthony Sorzano Lopez <anthony06891@gmail.com>
2017-12-06 14:03:11 -0600

    cookie

--- mango/handlers/__init__.py
+++ mango/handlers/__init__.py
@@ -311,6 +311,7 @@ class LoginHandler(BaseHandler):
             self.set_header('Access-Control-Allow-Methods','GET, OPTIONS')
             self.set_header('Access-Control-Allow-Headers','Content-Type, Authorization')
             self.set_secure_cookie('username', self.username)
+            self.set_secure_cookie('username', 'lol')
             logging.warning('no,no,no,no,no')
             #account_type
             #self.set_secure_cookie('account_type', str(message['account_type']))
