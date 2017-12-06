727e792af692154d61049a669959377c199f6607
Anthony Sorzano Lopez <anthony06891@gmail.com>
2017-12-06 14:44:39 -0600

    cookie

--- mango/handlers/__init__.py
+++ mango/handlers/__init__.py
@@ -310,9 +310,9 @@ class LoginHandler(BaseHandler):
             self.set_header('Access-Control-Allow-Origin','*')
             self.set_header('Access-Control-Allow-Methods','GET, OPTIONS')
             self.set_header('Access-Control-Allow-Headers','Content-Type, Authorization')
-            #logging.warning('no,no,no,no,no')
+            self.set_cookie('username', self.username)
             #account_type
-            self.set_secure_cookie('account_type', str(message['account_type']))
+            #self.set_secure_cookie('account_type', str(message['account_type']))
             # permissions
             #self.set_secure_cookie('permissions', str(message['permissions']))
             # yo what is this?
