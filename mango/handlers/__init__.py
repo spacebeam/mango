5a16e5bf3f4c9a9ad8050e759d380ef54c3ed5da
Anthony Solorzano Lopez <anthony06891@gmail.com>
2017-12-19 14:29:23 -0600

    update

--- mango/handlers/__init__.py
+++ mango/handlers/__init__.py
@@ -150,11 +150,11 @@ class BaseHandler(web.RequestHandler):
                     for (key, value) in response_doc.items()
                     if key not in IGNORE_ME
                 )
-
+            message.encode("utf-8")
         except Exception, e:
             logging.exception(e)
             raise gen.Return(e)
-        raise gen.Return(message.get('permissions', [].decode('utf-8')))
+        raise gen.Return(message.get('permissions', []))
 
     @gen.coroutine
     def get_account_uuid(self, account):
