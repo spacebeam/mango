25709e156298f9cefbf03087893326cac5577c00
Anthony Solorzano Lopez <anthony06891@gmail.com>
2018-01-19 14:32:18 -0600

    update

--- mango/system/__init__.py
+++ mango/system/__init__.py
@@ -43,7 +43,7 @@ def basic_authentication(handler_class):
                 return False
             logging.warning(auth_header[6:])
             #auth_decoded = base64.decodestring(auth_header[6:])
-            logging.warning(auth_decoded)
+            #logging.warning(auth_decoded)
             handler.username, handler.password = auth_decoded.split(':', 2)
             logging.info('%s enter the dungeon! @basic_auth' % handler.username)
             return True
