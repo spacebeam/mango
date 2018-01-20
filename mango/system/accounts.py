0e8d3a3d85df8e24fa53b90a87e1ea4099a75020
Anthony Solorzano Lopez <anthony06891@gmail.com>
2018-01-19 19:54:36 -0600

    update

--- mango/system/accounts.py
+++ mango/system/accounts.py
@@ -102,6 +102,9 @@ class Account(object):
         '''
         filter_status = 'status_register:active'
         order = 'sort=created_at_register+desc'
+        logging.warning(account)
+        accounts = account.decode('utf-8')
+        logging.warning(accounts)
         #filter_query = '(({0})AND({1}))&{2}'.format(filter_account, filter_status, order)
 
         # note where the hack change ' to %27 for the url string!
@@ -115,7 +118,7 @@ class Account(object):
         #logging.warning(urls)
         #urls.add(get_search_list(self.solr, search_index, query, fq_watchers, start_num, page_size))
   
-        if account.decode('utf8') is None:
+        if accounts is None:
             search_index = 'mango_account_index'
             query = 'uuid_register:*'
             filter_query = filter_status
@@ -125,18 +128,19 @@ class Account(object):
             logging.warning(urls)
             urls.add(get_search_list(self.solr, search_index, query, fq_watchers, start_num, page_size))
 
-        elif account.decode('utf8') is not None:
-            search_index = 'mango_account_index'
-            query = 'uuid_register:*'
+        elif accounts  is not None:
+            #search_index = 'mango_account_index'
+            #query = 'uuid_register:*'
             
-            filter_account = 'account_register:{0}'.format(account.decode('utf-8'))
+            #filter_account = 'account_register:{0}'.format(account.decode('utf-8'))
 
-            filter_query = '(({0})AND({1}))&{2}'.format(filter_account, filter_status, order)
-            fq_watchers = "watchers_register:*'{0}'*".format(account.decode('utf8')).replace("'",'%27')
-            urls = set()
-            urls.add(get_search_list(self.solr, search_index, query, filter_query, start_num, page_size))        
-            logging.warning(urls)
-            urls.add(get_search_list(self.solr, search_index, query, fq_watchers, start_num, page_size))
+            #filter_query = '(({0})AND({1}))&{2}'.format(filter_account, filter_status, order)
+            #fq_watchers = "watchers_register:*'{0}'*".format(account.decode('utf8')).replace("'",'%27')
+            #urls = set()
+            #urls.add(get_search_list(self.solr, search_index, query, filter_query, start_num, page_size))        
+            #logging.warning(urls)
+            logging.warning('no account')
+            #urls.add(get_search_list(self.solr, search_index, query, fq_watchers, start_num, page_size))
 
 
         # init got response list
