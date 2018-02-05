7297aa04ebf164a60dac249803e7f86c1e230088
Jean Chassoul <chassoul@gmail.com>
2018-02-05 15:24:54 -0500

    clean, clean, clean

--- mango/handlers/__init__.py
+++ mango/handlers/__init__.py
@@ -311,14 +311,7 @@ class LoginHandler(BaseHandler):
             self.set_header('Access-Control-Allow-Origin','*')
             self.set_header('Access-Control-Allow-Methods','GET, OPTIONS')
             self.set_header('Access-Control-Allow-Headers','Content-Type, Authorization')
-            
-            self.set_secure_cookie("username", self.username)
-            #account_type
-            logging.warning('test cookie!!!!!!')
-            self.set_secure_cookie('account_type', str(message['account_type']))
-            # permissions
-            self.set_secure_cookie('permissions', str(message['permissions']))
-            # yo what is this?
+            # Yo, missing set_header with the security token!
             self.username, self.password = (None, None)
             self.set_status(200)
             self.finish(message)
