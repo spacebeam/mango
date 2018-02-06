bf9fe076455170378059a9ef8e9ec9ed7ab4a84b
Jean Chassoul <chassoul@gmail.com>
2018-02-06 16:25:02 -0500

    orgs do not have password!

--- mango/handlers/accounts.py
+++ mango/handlers/accounts.py
@@ -372,8 +372,6 @@ class OrgsHandler(accounts.Account, BaseHandler):
         username = False
         # if the user don't provide an account we use the username
         account = (query_args.get('account', [username])[0] if not account else account)
-        # orgs do not have password!
-        struct['password'] = 'not_applicable'
         # execute new org struct
         org_uuid = yield self.new_org(struct)
         # complete message with receive uuid.
