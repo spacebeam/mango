7bfd957bad8ade023dd7ec35fe5b3367e52b9bc5
Jean Chassoul <chassoul@gmail.com>
2018-02-07 17:10:35 -0500

    clean, clean, clean

--- mango/handlers/accounts.py
+++ mango/handlers/accounts.py
@@ -64,7 +64,7 @@ class UsersHandler(accounts.Account, BaseHandler):
             # get cache data
             message = self.cache.get('accounts:{0}'.format(user_uuid))
             if message is not None:
-                logging.info('accounts:{0} done retrieving!'.format(user_uuid))
+                logging.info('cache accounts:{0} done retrieving!'.format(user_uuid))
                 self.set_status(200)
             else:
                 message = yield self.get_user(account, user_uuid)
