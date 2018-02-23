50fc1f411e6bee4f478143573ee3f675f0ba2283
Anthony Solorzano Lopez <anthony06891@gmail.com>
2018-02-23 10:47:37 -0600

    update

--- mango/tools/__init__.py
+++ mango/tools/__init__.py
@@ -58,8 +58,8 @@ def get_search_list(solr, search_index, query, filter_query, start_num, page_siz
         Build and return the list query url
     '''
     # note: the last replace smells a little funny...
-    return "http://172.31.1.11/search/query/{1}?wt=json&q={2}&fq={3}&start={4}&rows={5}&sort=created_at_register+desc".format(
-        solr, search_index, query, filter_query, start_num, page_size).replace(
+    return "http://172.31.1.11/search/query/{0}?wt=json&q={1}&fq={2}&start={3}&rows={4}&sort=created_at_register+desc".format(
+        search_index, query, filter_query, start_num, page_size).replace(
             ' ', '')
 
 def get_average(total, marks):
