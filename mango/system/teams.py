45d2a9f1eea944764b798ec3768317fbdaf053f7
Jean Chassoul <chassoul@gmail.com>
2018-03-20 18:49:40 -0400

    clean, clean, clean

--- mango/system/teams.py
+++ mango/system/teams.py
@@ -42,64 +42,6 @@ class Teams(object):
     '''
         Teams
     '''
-    @gen.coroutine
-    def quick_search(self, account, start, end, lapse, status, page_num, fields, search):
-        '''
-            Quick Seach
-        '''
-        search_index = 'mango_team_index'
-        query = 'allfields_register:{0}'.format(search.decode('utf-8'))
-
-        page_num = int(page_num)
-        page_size = self.settings['page_size']
-        start_num = page_size * (page_num - 1)
-
-        if not fields:
-            fields = 'name_register,description_register,assignees_register,source_register,destination_register,acknowledge_register,deadline_register,comments_register,created_by_register,last_update_by_register'
-        else:
-            fields = '{0}'.format(fields.decode('utf-8'))
-
-        url = quick_search_item(self.solr, search_index, query, start_num, page_size, fields).replace(' ', '')
-        
-        logging.warning('check this url')
-        logging.warning(url)
-        
-        IGNORE_ME = ["_yz_id","_yz_rk","_yz_rt","_yz_rb"]
-        got_response = []
-        # clean response message
-        message = {
-            'count': 0,
-            'page': page_num,
-            'results': []
-        }
-
-        def handle_request(response):
-            '''
-                Request Async Handler
-            '''
-            if response.error:
-                logging.error(response.error)
-                got_response.append({'error':True, 'message': response.error})
-            else:
-                got_response.append(json.loads(response.body))
-        try:
-            http_client.fetch(
-                url,
-                callback=handle_request
-            )
-            while len(got_response) == 0:
-                # don't be careless with the time.
-                yield gen.sleep(0.0021)
-            stuff = got_response[0]
-            if stuff['response']['numFound']:
-                message['count'] += stuff['response']['numFound']
-                for doc in stuff['response']['docs']:
-                    message['results'].append(clean_response(doc, IGNORE_ME))
-            else:
-                logging.error('there is probably something wrong!')
-        except Exception as error:
-            logging.warning(error)
-        return message
 
     @gen.coroutine
     def get_team(self, account, team_uuid):
@@ -498,8 +440,78 @@ class Teams(object):
         '''
             Remove team
         '''
-        # Yo, missing history ?
+        # So, still missing history ?
         struct = {}
         struct['status'] = 'deleted'
         message = yield self.modify_team(account, team_uuid, struct)
+        return message
+
+    # We really need this on all handlers?
+    @gen.coroutine
+    def quick_search(self, account, start, end, lapse, status, page_num, fields, search):
+        '''
+            Quick Seach
+        '''
+        search_index = 'mango_team_index'
+        page_num = int(page_num)
+        page_size = self.settings['page_size']
+        start_num = page_size * (page_num - 1)
+        query = 'allfields_register:{0}'.format(search.decode('utf-8'))
+        if not fields:
+            __fields = ('name_register',
+                        'description_register',
+                        'assignees_register',
+                        'source_register',
+                        'destination_register',
+                        'acknowledge_register',
+                        'deadline_register',
+                        'comments_register',
+                        'created_by_register',
+                        'last_update_by_register')
+            fields = ','.join(__fields)
+        else:
+            fields = '{0}'.format(fields.decode('utf-8'))
+
+        # WTF is this new quick_search_iteam?
+        url = quick_search_item(self.solr, search_index, query, start_num, page_size, fields).replace(' ', '')
+        
+        logging.warning("check this url inside tony's quick_search")
+        logging.warning(url)
+        
+        IGNORE_ME = ["_yz_id","_yz_rk","_yz_rt","_yz_rb"]
+        got_response = []
+        # clean response message
+        message = {
+            'count': 0,
+            'page': page_num,
+            'results': []
+        }
+
+        def handle_request(response):
+            '''
+                Request Async Handler
+            '''
+            if response.error:
+                logging.error(response.error)
+                got_response.append({'error':True, 'message': response.error})
+            else:
+                got_response.append(json.loads(response.body))
+
+        try:
+            http_client.fetch(
+                url,
+                callback=handle_request
+            )
+            while len(got_response) == 0:
+                # don't be careless with the time.
+                yield gen.sleep(0.0021)
+            stuff = got_response[0]
+            if stuff['response']['numFound']:
+                message['count'] += stuff['response']['numFound']
+                for doc in stuff['response']['docs']:
+                    message['results'].append(clean_response(doc, IGNORE_ME))
+            else:
+                logging.error('there is probably something wrong!')
+        except Exception as error:
+            logging.warning(error)
         return message
