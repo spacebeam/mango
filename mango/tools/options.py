5ebf8e169a3ac65295f99a4e69b5018ec77063f9
Jean Chassoul <jchassoul@gmail.com>
2018-09-05 18:04:37 -0500

    clean, clean, clean

--- mango/tools/options.py
+++ mango/tools/options.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-'''
-    Mango configuration server options.
-'''
 
 # This file is part of mango.
 
 # Distributed under the terms of the last AGPL License.
 # The full license is in the file LICENCE, distributed as part of this software.
+
 
 __author__ = 'Team Machine'
 
