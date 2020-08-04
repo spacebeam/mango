bf00fe76d0fc64d64d1edfad1622ee0f8d18f1ed
Jean Chassoul <jchassoul@gmail.com>
2020-08-04 14:34:36 -0600

    clean, clean, clean

---
+++ include/riak_codecs__init__.py
@@ -0,0 +1,27 @@
+import collections
+
+import riak.pb.messages
+
+from riak import RiakError
+from riak.codecs.util import parse_pbuf_msg
+from riak.util import bytes_to_str
+
+Msg = collections.namedtuple('Msg', ['msg_code', 'data', 'resp_code'])
+
+
+class Codec(object):
+    def parse_msg(self):
+        raise NotImplementedError('parse_msg not implemented')
+
+    def maybe_incorrect_code(self, resp_code, expect=None):
+        if expect and resp_code != expect:
+            raise RiakError("unexpected message code: %d, expected %d"
+                            % (resp_code, expect))
+
+    def maybe_riak_error(self, msg_code, data=None):
+        if msg_code == riak.pb.messages.MSG_CODE_ERROR_RESP:
+            if data is None:
+                raise RiakError('no error provided!')
+            else:
+                err = parse_pbuf_msg(msg_code, data)
+                raise RiakError(bytes_to_str(err.errmsg))
