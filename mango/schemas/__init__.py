bf17ca754b926a33386575031f95fb2e98142dcb
Jean Chassoul <jchassoul@gmail.com>
2020-08-04 21:03:50 -0600

    clean, clean, clean

--- mango/schemas/__init__.py
+++ mango/schemas/__init__.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
-
 # This file is part of mango.
 
 # Distributed under the terms of the last AGPL License.
-# The full license is in the file LICENCE, distributed as part of this software.
 
 
-__author__ = 'Team Machine'
+__author__ = 'Jean Chassoul'
 
 
 import arrow
-import uuid
 from schematics import models
 from schematics import types
 from schematics.types import compound
@@ -62,33 +58,6 @@ class RequiredBase(models.Model):
     created_at = types.TimestampType(default=arrow.utcnow().timestamp)
     last_update_by = types.StringType()
     last_update_at = types.TimestampType()
-
-
-class CleanBase(models.Model):
-    '''
-        Clean base class
-    '''
-    status = types.StringType()
-    account = types.StringType()
-    role = types.StringType()
-    email = types.EmailType()
-    phone_number = types.StringType()
-    extension = types.StringType()
-    country_code = types.StringType()
-    timezone = types.StringType()
-    company = types.StringType()
-    location = types.StringType()
-    phones = compound.ListType(compound.ModelType(Phone))
-    emails = compound.ListType(compound.ModelType(Email))
-    labels = types.DictType(types.StringType)
-    history = compound.ListType(types.StringType())
-    checked = types.BooleanType()
-    checked_by = types.StringType()
-    checked_at = types.TimestampType()
-    created_by = types.StringType()
-    created_at = types.TimestampType()
-    last_update_by = types.StringType()
-    last_update_at = types.TimestampType(default=arrow.utcnow().timestamp)
 
 
 class BaseResult(models.Model):
