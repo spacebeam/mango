df22a7340e3d931554554bc42b94fba51ef3632f
Jean Chassoul <jchassoul@gmail.com>
2020-08-04 19:44:21 -0600

    clean, clean, clean

--- mango/tools/__init__.py
+++ mango/tools/__init__.py
@@ -1,36 +1,23 @@
-# -*- coding: utf-8 -*-
-
 # This file is part of mango.
 
 # Distributed under the terms of the last AGPL License.
-# The full license is in the file LICENCE, distributed as part of this software.
 
 
-__author__ = 'Team Machine'
+__author__ = 'Jean Chassoul'
 
 
-import time
 import arrow
-import csv
 import ujson as json
 import logging
 import uuid
 from tornado import gen
 
 
-def check_new_accounts():
-    '''
-        Open issue about some Kong Admin API integration?
-    '''
-    pass
-
 def validate_uuid4(uuid_string):
     '''
-        Validate that a UUID string is in
-        fact a valid uuid4.
+        Validate that a UUID string is in fact a valid uuid4.
 
-        Happily, the uuid module does the actual
-        checking for us.
+        Happily, the uuid module does the actual checking.
     '''
     try:
         val = uuid.UUID(uuid_string, version=4)
@@ -40,36 +27,13 @@ def validate_uuid4(uuid_string):
         return False
     return str(val) == uuid_string
 
-def clean_response(response, ignore):
-    '''
-        clean response
-    '''
-    return dict(
-        (key.split('_register')[0], value)
-        for (key, value) in response.items()
-        if key not in ignore
-    )
 
-def get_search_item(solr, search_index, query, filter_query):
-    '''
-        Build and return the item query url
-    '''
-    return "https://{0}/search/query/{1}?wt=json&q={2}&fq={3}".format(
-        solr, search_index, query, filter_query
-    )
-
-def get_search_list(solr, search_index, query, filter_query, start_num, page_size):
-    '''
-        Build and return the list query url
-    '''
-    # LOL WTF CLEAN THIS, PUT STUFF ON TUPLES OR SOMETHING ...
-    return "https://{0}/search/query/{1}?wt=json&q={2}&fq={3}&start={4}&rows={5}&sort=created_at_register+desc, uuid_register+desc".format(solr, search_index, query, filter_query, start_num, page_size).replace(' ', '')
-    
 def get_average(total, marks):
     '''
         Get average from signals
     '''
     return float(total) / len(marks)
+
 
 def get_percentage(part, whole):
     '''
@@ -77,6 +41,7 @@ def get_percentage(part, whole):
 
     '''
     return "{0:.0f}%".format(float(part)/whole * 100)
+
 
 @gen.coroutine
 def check_json(struct):
@@ -86,9 +51,10 @@ def check_json(struct):
     try:
         message = json.loads(struct)
     except Exception as error:
-        message = json.dumps({'error':400})
+        message = json.dumps({'error': 400})
         raise error
     return message
+
 
 @gen.coroutine
 def check_times(start, end):
@@ -96,13 +62,15 @@ def check_times(start, end):
         Check times
     '''
     try:
-        start = (arrow.get(start) if start else arrow.get(arrow.utcnow().date()))
+        start = (arrow.get(start) if start
+                 else arrow.get(arrow.utcnow().date()))
         end = (arrow.get(end) if end else start.replace(days=+1))
-        message = {'start':start.timestamp, 'end':end.timestamp}
+        message = {'start': start.timestamp, 'end': end.timestamp}
     except Exception as error:
         logging.exception(error)
         raise error
     return message
+
 
 @gen.coroutine
 def check_times_get_timestamp(start, end):
@@ -110,13 +78,15 @@ def check_times_get_timestamp(start, end):
         Check times get timestamp
     '''
     try:
-        start = (arrow.get(start) if start else arrow.get(arrow.utcnow().date()))
+        start = (arrow.get(start) if start
+                 else arrow.get(arrow.utcnow().date()))
         end = (arrow.get(end) if end else start.replace(days=+1))
-        message = {'start':start.timestamp, 'end':end.timestamp}
+        message = {'start': start.timestamp, 'end': end.timestamp}
     except Exception as error:
         logging.exception(error)
-        raise error    
+        raise error
     return message
+
 
 @gen.coroutine
 def check_times_get_datetime(start, end):
@@ -124,13 +94,15 @@ def check_times_get_datetime(start, end):
         Check times get datetime
     '''
     try:
-        start = (arrow.get(start) if start else arrow.get(arrow.utcnow().date()))
+        start = (arrow.get(start) if start
+                 else arrow.get(arrow.utcnow().date()))
         end = (arrow.get(end) if end else start.replace(days=+1))
-        message = {'start':start.naive, 'end':end.naive}
+        message = {'start': start.naive, 'end': end.naive}
     except Exception as error:
         logging.exception(error)
         raise error
     return message
+
 
 def clean_message(struct):
     '''
@@ -138,11 +110,11 @@ def clean_message(struct):
     '''
     struct = struct.to_native()
     struct = {
-        key: struct[key]
-            for key in struct
-                if struct[key] is not None
+        key: struct[key] for key in struct
+        if struct[key] is not None
     }
     return struct
+
 
 def clean_structure(struct):
     '''
@@ -150,11 +122,11 @@ def clean_structure(struct):
     '''
     struct = struct.to_primitive()
     struct = {
-        key: struct[key]
-            for key in struct
-                if struct[key] is not None
+        key: struct[key] for key in struct
+        if struct[key] is not None
     }
     return struct
+
 
 def clean_results(results):
     '''
@@ -164,31 +136,15 @@ def clean_results(results):
     results = results.get('results')
     results = [
         {
-            key: dic[key]
-                for key in dic
-                    if dic[key] is not None
+            key: dic[key] for key in dic
+            if dic[key] is not None
         } for dic in results
     ]
     return {'results': results}
+
 
 def str2bool(boo):
     '''
         String to boolean
     '''
     return boo.lower() in ('yes', 'true', 't', '1')
-
-def export_to_csv(csv_file_path, dict_data):
-    '''
-        Tony's first attemp on general export_to_csv function
-        TODO: Clean/finish it?
-    '''
-    csv_file = open(csv_file_path, 'w')
-    writer = csv.writer(csv_file, dialect='excel')
-    headers = dict_data[0].keys()
-    writer.writerow(headers)
-    for dat in dict_data:
-        line = []
-        for field in headers:
-            line.append(dat[field])
-        writer.writerow(line)
-    csv_file.close()
