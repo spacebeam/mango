9a168cc4a7b027a0f35d44173b6992114bdd53e5
Jean Chassoul <jchassoul@gmail.com>
2020-08-05 11:49:41 -0600

    clean, clean, clean

--- mango/system/__init__.py
+++ mango/system/__init__.py
@@ -1,2 +1,10 @@
 # system can be renamed to `subroutine` or `subroutines` directory
 # to match the schemas and handlers directory pattern, but.. that's that.
+
+
+def update_struct():
+    pass
+
+
+def remove_struct():
+    pass
